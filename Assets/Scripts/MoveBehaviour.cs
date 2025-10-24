993ce9277946b23f8fda6f33529d4a2fa801a126
Sergi <sergi.albalat.7e8@itb.cat>
2025-10-24 15:32:08 +0200

    Player Run Animation Added

--- Assets/Scripts/MoveBehaviour.cs
+++ Assets/Scripts/MoveBehaviour.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-
 public class MoveBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private AnimationBehaviour _animation;
     [SerializeField] private float speed = 3f;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animation = GetComponent<AnimationBehaviour>();
     }
     public void Move(Vector2 direction)
     {
         _rb.linearVelocity = direction.normalized * speed;
+        _animation.RunAnimation(direction);
     }
 }
