f6980c1f60546aa0116a528c23f86c381ad2c278
Sergi <sergi.albalat.7e8@itb.cat>
2025-11-20 16:03:44 +0200

    Level3MoreDifficultWithBulletSatck

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : MonoBehaviour
+{
+    [SerializeField] private Bullet bulletPrefab;
+    private Stack<Bullet> bullets = new Stack<Bullet>();
+    private void Start()
+    {
+        InvokeRepeating("ShootBullet", 0, 1);
+    }
+    private Bullet ShootBullet()
+    {
+        Debug.Log(bullets.Count);
+        if(bullets.Count > 0)
+        {
+            Bullet bullet = bullets.Pop();
+            bullet.transform.position = transform.position;
+            bullet.gameObject.SetActive(true);
+            return bullet;
+        }
+        else
+        {
+            Bullet bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = transform.position;
+            return bullet;
+        }
+    }
+    public void StoreBullet(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+        bullets.Push(bullet);
+    }
+}
